1a583ba7407693f09010e9422da2f33d6311dd10
Jagadeesh <Jagadeesh.jay88@gmail.com>
2022-12-17 15:00:47 +0530

    Commit by Monika

--- src/main/java/Sample/demo1.java
+++ src/main/java/Sample/demo1.java
@@ -7,13 +7,22 @@ public class demo1 {
 	@Test
 	public void sampleTest()
 	{
+		
+		
 		System.out.println("Hi this is sample");
+		
+		
 		
 		System.out.println("this is Jagadeesh");
 		
 		
 		
+		
+		
 		System.out.println("This is Monika");
+		
+		
+		
 		
 		
 	}
